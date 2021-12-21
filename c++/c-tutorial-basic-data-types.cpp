f569c137a2bc0bb752cee2c9105c5ca6dd57a431
Lam <76472734+ngld0@users.noreply.github.com>
2021-12-21 21:55:22 +0800

    Create c-tutorial-basic-data-types.cpp

---
+++ c++/c-tutorial-basic-data-types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    std::cin>> a >> b>>c>>d>>e;
+    
+    printf("%d\n%ld\n%c\n%f\n%lf\n",a,b,c,d,e);
+    //std::cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<"\n"<<e<<std::endl;
+    return 0;
+}
