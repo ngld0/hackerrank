184db3992f73a4b65db8782439565eb80775b3da
Lam <76472734+ngld0@users.noreply.github.com>
2021-12-31 14:31:27 +0800

    Create happy-ladybugs.cpp

---
+++ problems/happy-ladybugs.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'happyLadybugs' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING b as parameter.
+ */
+
+string happyLadybugs(string b) {
+    if(b.length() == 1 && b[0]!= '_') return "NO";
+    unordered_map<char,int> mm;
+    
+    for(int i = 0 ;i < b.length();i++){
+        mm[b[i]]++;
+    }
+    auto find = mm.find('_');
+    if(find != mm.end()){
+        
+        for(auto it : mm){
+            
+            if(it.first != '_' && it.second <2) return "NO";
+        }
+    }else {
+        for(int i = 1; i< b.length();i++){
+            if(b[i-1] == b[i] || b[i+1] == b[i]){
+                continue;
+            }
+            return "NO";
+            
+        }
+        
+    }
+    
+    return "YES";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string g_temp;
+    getline(cin, g_temp);
+
+    int g = stoi(ltrim(rtrim(g_temp)));
+
+    for (int g_itr = 0; g_itr < g; g_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string b;
+        getline(cin, b);
+
+        string result = happyLadybugs(b);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
