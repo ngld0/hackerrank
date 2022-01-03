5240b9942ba9333f0f07f9fae9407ad0384ba2b5
Lam <76472734+ngld0@users.noreply.github.com>
2022-01-03 21:05:02 +0800

    Create 3d-surface-area.cpp

---
+++ problems/3d-surface-area.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'surfaceArea' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY A as parameter.
+ */
+
+int surfaceArea(vector<vector<int>> A) {
+    // rows
+    int h = A.size();
+    //columns
+    int w = A[0].size();
+    //botton and top surface
+    int s = 2 * h * w;
+    // sum all another 4  surface of one cube
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            s += A[i][j] * 4;
+        }
+    }
+    // s is total, then we have to decrease the surfaces that we dont see
+    for (int i = 0; i < h; i++) {
+        for (int j = 1; j < w; j++) {
+            s -= min(A[i][j-1], A[i][j]) * 2;
+        }
+    }
+    
+    for (int i = 1; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            s -= min(A[i-1][j], A[i][j]) * 2;
+        }
+    }
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int H = stoi(first_multiple_input[0]);
+
+    int W = stoi(first_multiple_input[1]);
+
+    vector<vector<int>> A(H);
+
+    for (int i = 0; i < H; i++) {
+        A[i].resize(W);
+
+        string A_row_temp_temp;
+        getline(cin, A_row_temp_temp);
+
+        vector<string> A_row_temp = split(rtrim(A_row_temp_temp));
+
+        for (int j = 0; j < W; j++) {
+            int A_row_item = stoi(A_row_temp[j]);
+
+            A[i][j] = A_row_item;
+        }
+    }
+
+    int result = surfaceArea(A);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
